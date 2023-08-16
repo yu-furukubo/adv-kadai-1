e812b06f478085044caeaa742f65a58d4fe8c379
yu-furukubo <138659773+yu-furukubo@users.noreply.github.com>
2023-08-16 02:08:41 +0000

    [finish]kadai-4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,4 @@
 class BooksController < ApplicationController
-  def top
-  end
-
   def index
     @books = Book.all
     @book = Book.new
@@ -13,6 +10,7 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
@@ -27,7 +25,7 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
